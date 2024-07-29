8fe80298aca2ac7a07c5d27df3a13de8bf05918c
Nemesh Dima <dimitrijnemesh@gmail.com>
2024-07-29 15:51:44 +0300

    configured eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,17 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        useTabs: true,
+        semi: true,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        printWidth: 100,
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
