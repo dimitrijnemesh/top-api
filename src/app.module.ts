5cd2be3bac68c110af66f9845867eec2fc61b6d9
Nemesh Dima <dimitrijnemesh@gmail.com>
2024-07-30 23:20:08 +0300

    Added mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,17 @@ import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, TopPageModule, ProductModule, ReviewModule],
+	imports: [
+		ConfigModule.forRoot(),
+		MongooseModule.forRoot('mongodb://localhost/test'),
+		AuthModule,
+		TopPageModule,
+		ProductModule,
+		ReviewModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
