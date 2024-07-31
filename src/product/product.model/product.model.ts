04ee55f43a76e5fe00e0c9fac59a51a0742e9788
Nemesh Dima <dimitrijnemesh@gmail.com>
2024-07-31 16:30:07 +0300

    Added ProductSchema to Database

--- src/product/product.model/product.model.ts
+++ src/product/product.model/product.model.ts
@@ -1,17 +1,54 @@
-export class ProductModel {
-	_id: string;
-	image: string;
-	title: string;
-	price: number;
-	oldPrice: number;
-	credit: number;
-	calculatedRating: number;
-	description: string;
-	advantages: string;
-	disAdvantages: string;
-	categories: string[];
-	tags: string;
-	characteristics: {
-		[key: string]: string;
-	};
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ProductDocument = HydratedDocument<ProductModel>;
+
+@Schema()
+class ProductCharacteristics {
+	@Prop({ required: true })
+	name: string;
+
+	@Prop({ required: true })
+	value: string;
 }
+
+@Schema({ timestamps: true })
+export class ProductModel {
+	@Prop({ required: true })
+	image: string;
+
+	@Prop({ required: true })
+	title: string;
+
+	@Prop({ required: true })
+	price: number;
+
+	@Prop({ required: true })
+	oldPrice: number;
+
+	@Prop({ required: true })
+	credit: number;
+
+	@Prop({ required: true })
+	calculatedRating: number;
+
+	@Prop({ required: true })
+	description: string;
+
+	@Prop({ required: true })
+	advantages: string;
+
+	@Prop({ required: true })
+	disAdvantages: string;
+
+	@Prop({ required: true, type: [String] })
+	categories: string[];
+
+	@Prop({ required: true, type: [String] })
+	tags: string[];
+
+	@Prop({ required: true, type: [ProductCharacteristics], _id: false })
+	characteristics: ProductCharacteristics[];
+}
+
+export const ProductSchema = SchemaFactory.createForClass(ProductModel);
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductModel, ProductSchema } from './product.model/product.model';
 
 @Module({
+	imports: [MongooseModule.forFeature([{ name: ProductModel.name, schema: ProductSchema }])],
 	controllers: [ProductController],
 })
 export class ProductModule {}
