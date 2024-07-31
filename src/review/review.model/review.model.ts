cb97156e98695ddad26900294f0a305889a7af24
Nemesh Dima <dimitrijnemesh@gmail.com>
2024-07-31 16:39:20 +0300

    Added ReviewSchema to Database

--- src/review/review.model/review.model.ts
+++ src/review/review.model/review.model.ts
@@ -1,8 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ProductDocument = HydratedDocument<ReviewModel>;
+
+@Schema({ timestamps: true })
 export class ReviewModel {
-	_id: string;
+	@Prop({ required: true })
 	name: string;
+
+	@Prop({ required: true })
 	title: string;
+
+	@Prop({ required: true })
 	descriptoin: string;
+
+	@Prop({ required: true })
 	rating: number;
+
+	@Prop({ required: true })
 	createdAt: Date;
 }
+
+export const ReviewSchema = SchemaFactory.createForClass(ReviewModel);
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ReviewController } from './review.controller';
+import { ReviewModel, ReviewSchema } from './review.model/review.model';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+	imports: [MongooseModule.forFeature([{ name: ReviewModel.name, schema: ReviewSchema }])],
 	controllers: [ReviewController],
 })
 export class ReviewModule {}
