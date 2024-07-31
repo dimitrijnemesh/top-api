eacff9af166a63e14b0f18a68bac08c90fa51b81
Nemesh Dima <dimitrijnemesh@gmail.com>
2024-07-31 15:44:39 +0300

    Added AuthSchema to Database

--- src/auth/auth.model/auth.model.ts
+++ src/auth/auth.model/auth.model.ts
@@ -1,4 +1,15 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type AuthDocument = HydratedDocument<AuthModel>;
+
+@Schema({ timestamps: true })
 export class AuthModel {
+	@Prop({ required: true, unique: true })
 	email: string;
+
+	@Prop({ required: true })
 	passwordHash: string;
 }
+
+export const AuthSchema = SchemaFactory.createForClass(AuthModel);
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModel, AuthSchema } from './auth.model/auth.model';
 
 @Module({
+	imports: [MongooseModule.forFeature([{ name: AuthModel.name, schema: AuthSchema }])],
 	controllers: [AuthController],
 })
 export class AuthModule {}
