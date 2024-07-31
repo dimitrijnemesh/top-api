08c1bbc4dc22e3421d8e7a87c5787f9ad96299d2
Nemesh Dima <dimitrijnemesh@gmail.com>
2024-07-31 14:47:37 +0300

    Added mongo config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,18 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
-		MongooseModule.forRoot('mongodb://localhost/test'),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig,
+		}),
 		AuthModule,
 		TopPageModule,
 		ProductModule,
